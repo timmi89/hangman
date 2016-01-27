5217b188595cf8fb47e733bf5d12a56f044ab835
andrew tellerai and timothea mcguire <timothea.mcguire@gmail.com>
2016-01-27 14:23:03 -0800

    added 3 passing specs, object, dash, letter checker

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
-function Task(name, description) {
-  this.name = name;
-  this.description = description;
+function gameObj() {
+  // var words = ["allosaurus", "brachiosaurus", "diplodocus", "iguanodon", "stegosaurus", "triceratops", "velociraptor", "ankylosaurus"]; will be used, just not for specs
+  var words = ["allosaurus"]; //array should be empty, allo' is for specs
+  this.randomWord = words[Math.floor(Math.random() * words.length)];
+  this.randomWordArray = this.randomWord.split('');
 };
 
-$(function(){
-  $("form#newTask").submit(function(event){
-    event.preventDefault();
-    // debugger;
-    var inputtedTaskName = $("input#newTaskName").val();
-    var inputtedTaskDescription = $("input#newTaskDescription").val();
-    var newTask = new Task(inputtedTaskName, inputtedTaskDescription);
+gameObj.prototype.lettersSwap = function() {
+  this.output = this.randomWord.replace(/[abcdefghijklmnopqrstuvwxyz]/ig, "_ ");
+  return this.output;
+};
 
-    $("form#tasks").append("<p><input type='checkbox' id='newTask.name'>" + newTask.name + "<br></p>");
 
-    $("input#newTaskName").val("");
-    $("input#newTaskDescription").val("");
+gameObj.prototype.guess = function(letter) {
 
-  });
+  for ( var i = 0; i < this.randomWordArray.length; i++) {
+    if (this.randomWordArray[i] === letter) {
+      console.log(true);
+      return true;
+    } else {
+      console.log(false);
+      return false;
+    }
+  };
+};
 
-  $("#tasks").click(function() {
-    $("input:checked").parent().remove();
-  });
 
-});
+
+
+// $(document).ready(function() {
+//   $("form#inquiry").submit(function(event) {
+//     var word = $("input#userInput").val();
+//     var result = vowelSwap(word);
+//
+//   // $("span#result").show();
+//   document.write(result);
+//   $("span#puzzle").text(result);
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,20 @@
-describe('To Do List', function() {
-
-  it("adds the name of a task to the to do list", function() {
-    var testTask = new Task("Homework", "Read pages 1-3");
-    expect(testTask.name).to.equal("Homework");
-    expect(testTask.description).to.equal("Read pages 1-3");
+describe('gameObj', function() {
+  it("Creates a game object with a random word and a split version of the word.", function() {
+    var newGame = new gameObj();
+    // expect(newGame.randomWord).to.be.a('string');
+    // expect(newGame.randomWordArray).to.be.a('array');
+    expect(newGame.randomWord).to.equal('allosaurus');
+    expect(newGame.randomWordArray).to.eql(['a','l','l','o', 's','a','u','r','u','s']);
   });
-
-  // it("adds the full Name to a contact", function() {
-  //   var testContact = new Contact("Bill","Clinton");
-  //   expect(testContact.fullName()).to.equal("Bill Clinton");
-  // });
-
+  it("Creates a new string that replaces the letters with underscores.", function() {
+    var newGame = new gameObj();
+    expect(newGame.lettersSwap('allosaurus')).to.equal('_ _ _ _ _ _ _ _ _ _ ');
+    // it("Will replace letters with underscores.", function() {   var newGame = new gameObj();
+    //   expect(newGame.lettersSwap()).to.equal('_ ');
+    // });
+  });
+  it("Checks the users letter that they guessed to see if it's correct", function() {
+    var newGame = new gameObj();
+    expect(newGame.guess('a')).to.equal(true);
+  });
 });
